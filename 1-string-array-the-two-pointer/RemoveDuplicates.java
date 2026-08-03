8faecd0f9fba0d9ec565942ce91a695fd511a9f0
SurajChauhan76 <suraj.chauhan7670@outlook.com>
2026-08-03 08:59:33 +0530

    Updated - RemoveDuplicates

--- 1-string-array-the-two-pointer/RemoveDuplicates.java
+++ 1-string-array-the-two-pointer/RemoveDuplicates.java
@@ -24,8 +24,13 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
         // int [] arr = {1, 1, 2, 3, 5, 6, 7, 2, 1}; // This array is not sorted and it results in incorrect output
         int [] arr = {1, 1, 1, 2, 2, 3, 5, 6, 7};  // Ensure the array is sorted
-        int res = removeDuplicates(arr);
+        int res = removeDuplicates(arr); // Gives the length of the unique array
         System.out.println(res);
+
+        // arr = {1, 2, 3, 5, 6, 7, 5, 6, 7};  // Upto index 6 array contains the unique values but after that it contains the Garbage as array size can't be shrink hence after index 6 the same elements from original array are copied.
         System.out.println(Arrays.toString(arr));
+
+        int[] uniqueArr = Arrays.copyOfRange(arr, 0, res);
+        System.out.println(Arrays.toString(uniqueArr));
     }
 }
